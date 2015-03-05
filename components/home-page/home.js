0dc8e0c65290b55b353e5cd4212c245d640974e5
uncleoptimus <uncle.optimus@gmail.com>
2015-03-04 22:04:11 -0800

    spotify now cross queries musix to obtain lyrics url for result. adjusted styles for audio player and delegated controls to page body

--- components/home-page/home.js
+++ components/home-page/home.js
@@ -11,14 +11,14 @@ define(["knockout", "text!./home.html", "knockout-postbox"], function(ko, homeTe
 
 		self.search_results = ko.observableArray(test_list).publishOn('new_results');
 
-
+		// update map by calling google place search
 		self.goSearch = function() {
-			// update map by calling google place search
-//				mapView.doPlaceSearch(self.search_terms());
-			console.log("Search terms are...%s", self.search_terms());
 			self.message("Searching for..." + self.search_terms());
 
-			var loc = app.doPlaceSearch(self.search_terms());
+			// lets simplify the search terms shall we?
+			var simple_terms = self.search_terms().split(/\s|,/g, 2).join(' ');
+			console.log("Modified search terms are...%s", simple_terms);
+			var loc = app.doPlaceSearch();
 			console.log("New location is at...%O", loc);
 
 			// format search string for api query
@@ -29,31 +29,48 @@ define(["knockout", "text!./home.html", "knockout-postbox"], function(ko, homeTe
 		};
 
 		// look for songs on spotify
+		// NOTE I saw a number of repeat results so I
+		// run the response through a filter
 		function spotifySearch(formatted_terms) {
 			var spotify_query = 'https://api.spotify.com/v1/search?q=' + formatted_terms + '&type=track&limit=10';
 
 			$.getJSON(spotify_query, function(data) {
 				var track_list = data.tracks.items; // an array
 				console.log("Response from Spotify...");
-				console.log("This spotify track object...%O", track_list[0]);
-
 				track_list.forEach(function(track) {
 					var track_name = track.name;
 					var track_artist = track.artists[0].name;
 					var track_cover = track.album.images[2].url;
 					var track_url = track.preview_url;
+
+					// check to see if this result already exists
 					if(!results_buffer.alreadyInArray(track_name, track_artist)) {
-						results_buffer.push(new Result(track_name, track_artist, track_cover, track_url));
+						// fetch a lyrics url from musixmatch
+						// using the title and name from spotify result
+						// NOTE: must be a nested async call
+						var track_lyrics;
+						var musix_query = 'http://api.musixmatch.com/ws/1.1/track.search?q_track=' + track_name + '&q_artist=' + track_artist + '&f_has_lyrics=1&apikey=0bc726067d82f809bd3d1f7b5f0f7c2c';
+						$.getJSON(musix_query, function(data) {
+							var fetch_result = data.message.body.track_list;
+							if (fetch_result.length > 0) {
+								var track = fetch_result.track_list[0];
+								track_lyrics = track.track.track_share_url;
+							}
+							// finally we can push the results
+							self.search_results.push(new Result(track_name, track_artist, track_cover, track_url, track_lyrics));
+						}).error(function(e) {
+							self.message("Uh-oh! Problem with MusixMatch track fetch!");
+						});
 					}
 				});
-
-				self.search_results(results_buffer);
 			}).error(function(e) {
 				self.message("Aw man! Problem with Spotify!");
 			});
 		}
 
 		// look for songs on musixmatch
+		// NOTE I did not see repeat results from musix queries
+		// so I did not run response through a filter
 		function musixSearch(formatted_terms) {
 			var musix_query = 'http://api.musixmatch.com/ws/1.1/track.search?q_lyrics=' + formatted_terms + '&f_has_lyrics=1&s_track_rating=ASC&f_lyrics_language=en&apikey=0bc726067d82f809bd3d1f7b5f0f7c2c';
 
@@ -63,32 +80,20 @@ define(["knockout", "text!./home.html", "knockout-postbox"], function(ko, homeTe
 				track_list.forEach(function(track) {
 					var track_name = track.track.track_name;
 					var track_artist = track.track.artist_name;
-
 					var track_lyrics = track.track.track_share_url;
-//					var track_lyrics = track.track.track_edit_url;
-
 					var track_cover = track.track.album_coverart_100x100;
-//					console.log("The cover value for %s is %s", track_name, track_cover);
 
-
-					results_buffer.push(new Result(track_name, track_artist, track_cover, track_lyrics));
-
-
+					// push to the results buffer to minimize updates
+					// on observable, reduce page redraws
+					results_buffer.push(new Result(track_name, track_artist, track_cover, undefined, track_lyrics));
 				});
-
 				// finally, upate the actual observable in one go
 				// AFTER async call returns
-
-				// should filter the array here for same results
-				console.log("results buffer has %s items", results_buffer.length);
-
-				console.log("Updating search results!!!");
 				self.search_results(results_buffer);
 			}).error(function(e) {
 				self.message("Uh-oh! Problem with MusixMatch!");
 			});
 		}
-
 	}
 
 	return { viewModel: HomeViewModel, template: homeTemplate };
