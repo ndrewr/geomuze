52aa06cc6c7e8807136ef0055311525917b69d4b
uncleoptimus <uncle.optimus@gmail.com>
2015-03-17 02:09:36 -0700

    started implementing build process, concat and uglifying files, saving on http requests etc...

--- app/app.js
+++ app/app.js
@@ -230,7 +230,7 @@
 		// is displayed on map
 		self.configInfopane = function(track) {
 			var audio_template =
-					'<div id="jukebox"><div class="player-info">Sample Clip</div><a class="audio-control play icon-play3" href="#"><span>Play</span></a><div class="loader"><div class="play-progress"></div></div><audio class="aud" src="http://www.scottandrew.com/mp3/demos/holding_back_demo_011504.mp3"><p>Oops, looks like your browser does not support HTML 5 audio.</p></audio></div>';
+					'<div id="jukebox"><div class="player-info">Sample Clip</div><a class="audio-control play icon-play3" href="#"><span>Play</span></a><div class="loader"><div class="play-progress"></div></div><audio class="aud" src=""><p>Oops, looks like your browser does not support HTML 5 audio.</p></audio></div>';
 
 			var info_template = '<h3>' + track.track_name +
 					'</h3><p>' + track.artist_name +
